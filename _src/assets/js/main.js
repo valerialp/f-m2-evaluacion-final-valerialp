57fc0b0b23315c5985e2ad27ffd0801fc5f6196f
valerialp <valeria.lorentep@gmail.com>
2019-04-22 15:37:51 +0200

    link js and make const and event

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-console.log('>> Ready :)');
+console.log('>> Ready');
+
+const inputSearchEl = document.querySelector('.search-bar');
+const buttonSearchEl = document.querySelector('.btn-search');
+const listResultEl = document.querySelector('.search-list');
+
+function handlerButtonClick(){
+    
+}
+
+buttonSearchEl.addEventListener('click', handlerButtonClick);
